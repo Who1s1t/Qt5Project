822c33065d76f378167695c9d0c85ea462fb57a5
OVER <dedsek965@gmail.com>
2022-11-10 07:16:31 +0300

    update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import sqlite3
 import sys
+import datetime
 import requests
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget
@@ -16,6 +18,7 @@ def convert(value, from_, to):
 
 class App(QWidget):
     def __init__(self):
+        self.con = sqlite3.connect("convert.db")
         self.start()
         self.set()
 
@@ -26,17 +29,33 @@ class App(QWidget):
     def set(self):
         data = requests.get(
             'https://openexchangerates.org/api/latest.json?app_id=2db968ef5bca471f93c0c0e86217a9d5').json()
-        data = data['rates'].keys()
+        data_db = data['rates']
+        data = data_db.keys()
         self.ui.b.addItems(data)
         self.ui.b_2.addItems(data)
         self.ui.iz.setText("сколько")
         self.ui.v.setText("получите")
         self.ui.but.setText("Конвертировать")
+        self.ui.bupdate.setText("Обновить Базу Данных")
         self.ui.but.clicked.connect(lambda: self.display())
+        self.ui.bupdate.clicked.connect(lambda: self.update_db())
 
     def display(self):
         res = convert(int(self.ui.iz.text()), str(self.ui.b.currentText()), str(self.ui.b_2.currentText()))
         self.ui.v.setText(str(res))
+
+    def update_db(self):
+        data = requests.get(
+            'https://openexchangerates.org/api/latest.json?app_id=2db968ef5bca471f93c0c0e86217a9d5').json()
+        data_db = data['rates']
+        time = datetime.datetime.now()
+        self.con.cursor().execute(f"""DELETE from course""")
+        for i in data_db.items():
+            iso_code = i[0]
+            value = i[1]
+            self.con.cursor().execute(
+                f"""INSERT INTO course (iso_code,value_,update_) VALUES ('{iso_code}',{value},'{time}')""")
+        self.con.commit()
 
 
 if __name__ == '__main__':
