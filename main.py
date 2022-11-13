842d4f9d38d3d858a4b7ce124ca8ed98fa701d2d
OVER <dedsek965@gmail.com>
2022-11-14 01:34:16 +0300

    add full db

--- main.py
+++ main.py
@@ -6,19 +6,10 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget
 
 
-def convert(value, from_, to):
-    data_from = requests.get(
-        f'https://openexchangerates.org/api/latest.json?app_id=2db968ef5bca471f93c0c0e86217a9d5&symbols={from_}').json()
-    data_to = requests.get(
-        f'https://openexchangerates.org/api/latest.json?app_id=2db968ef5bca471f93c0c0e86217a9d5&symbols={to}').json()
-    from_USD = value / data_from["rates"][from_]
-    res = data_to["rates"][to] * from_USD
-    return res
-
-
 class App(QWidget):
     def __init__(self):
         self.con = sqlite3.connect("convert.db")
+        self.cur = self.con.cursor()
         self.start()
         self.set()
 
@@ -27,10 +18,8 @@ class App(QWidget):
         self.ui.show()
 
     def set(self):
-        data = requests.get(
-            'https://openexchangerates.org/api/latest.json?app_id=2db968ef5bca471f93c0c0e86217a9d5').json()
-        data_db = data['rates']
-        data = data_db.keys()
+        data1 = self.cur.execute("""SELECT iso_code FROM course""").fetchall()
+        data = [i[0] for i in data1]
         self.ui.b.addItems(data)
         self.ui.b_2.addItems(data)
         self.ui.iz.setText("сколько")
@@ -41,7 +30,7 @@ class App(QWidget):
         self.ui.bupdate.clicked.connect(lambda: self.update_db())
 
     def display(self):
-        res = convert(int(self.ui.iz.text()), str(self.ui.b.currentText()), str(self.ui.b_2.currentText()))
+        res = self.convert(float(self.ui.iz.text()), str(self.ui.b.currentText()), str(self.ui.b_2.currentText()))
         self.ui.v.setText(str(res))
 
     def update_db(self):
@@ -49,13 +38,22 @@ class App(QWidget):
             'https://openexchangerates.org/api/latest.json?app_id=2db968ef5bca471f93c0c0e86217a9d5').json()
         data_db = data['rates']
         time = datetime.datetime.now()
-        self.con.cursor().execute(f"""DELETE from course""")
+        self.cur.execute(f"""DELETE from course""")
         for i in data_db.items():
             iso_code = i[0]
             value = i[1]
-            self.con.cursor().execute(
+            self.cur.execute(
                 f"""INSERT INTO course (iso_code,value_,update_) VALUES ('{iso_code}',{value},'{time}')""")
         self.con.commit()
+
+    def convert(self, value, from_, to):
+        data_from = self.cur.execute(f"""SELECT value_ FROM course WHERE iso_code = '{from_}'""").fetchall()
+        data_from = data_from[0][0]
+        data_to = self.cur.execute(f"""SELECT value_ FROM course WHERE iso_code = '{to}'""").fetchall()
+        data_to = data_to[0][0]
+        from_USD = value / data_from
+        res = data_to * from_USD
+        return res
 
 
 if __name__ == '__main__':
